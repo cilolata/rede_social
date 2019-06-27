11251343daed3a840e12bc6c048599b1315f7f00
ciça <48863588+cilolata@users.noreply.github.com>
2019-06-27 16:51:14 -0300

    Add files via upload

---
+++ rede_social/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--bootstrap css-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projeto Rede Social</title>
+</head>
+
+<body>
+    <!--inicio cabeçalho-->
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light">
+             <div class="container">
+                <a href="index.php" class="navbar-brand">
+                    <img src="img/light-bulb.png" alt="imagem_logo" class="">
+                    Nome da Rede
+                </a>
+                    <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-principal">
+                         <span class="navbar-toggler-icon"></span>
+                    </button>
+                <div class="collapse navbar-collapse" id="nav-principal">
+                    <ul class="navbar-nav list-unstyle ml-auto">
+                        <li class="nav-item"><a href="#" class= "btn btn-outline">Cadastre-se</a><li>
+                        <li class="nav-item ml-4"><a href="#" class="btn btn-outline">Entrar</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!--fim cabeçalho-->
+    </header>
+
+    <!-- inicio section destaque-->
+    <section id="home" class="">
+        <div class="jumbotron jumbotron-fluid bg-light">
+            <div class="row">
+                <div class="col-8">
+                    <h1 class="display-4">Conecte-se com pessoas que querem mudar o mundo</h1>
+                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat obcaecati sint hic maiores est deleniti consectetur minima asperiores dolorum quas nulla consequatur, eligendi veritatis, culpa ea iure suscipit ducimus deserunt!</p>
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Leia mais</a>
+                </div>
+
+                <div class="col-4 d-sm-none d-md-block">
+                    <img src="img/ecologia.png" alt="imagem mãos segurando a terra" class="ml-200">
+                </div> 
+                   
+            </div>        
+        </div>
+    </section>
+    <!-- fim section destaque-->
+
+    <section>
+        
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<footer>
+<div>Icons made by <a href="https://www.flaticon.com/authors/mynamepong" title="mynamepong">mynamepong</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://br.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+</footer>
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+</body>
+
+</html>
