ab8ec8cf8736fe14d56110fa88636aaf80eb3c55
Lais Guttilla <laisguttilla@gmail.com>
2019-07-23 21:33:20 -0300

    contact-us + modificacoes

--- header.php
+++ header.php
@@ -12,32 +12,45 @@
                 <!--fim cabeçalho logo e nome-->
 
                 <!--menu collapse-->
-                <button class="navbar-toggler" data-toggle="collapse" data-target="#cadastro_navbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <div class="dropdown d-xl-none d-lg-none">
+                    <button class="btn btn-outline-success dropdown-toggle" data-toggle="dropdown" type="button"> Sign up </button>
+                    <div class="dropdown-menu">
+                        <form class="px-4 py-3" role="login" method="POST">
+                            <div class="form-group m-1">
+                                <input class="form-control rounded" type="text" name="email" class="emailTop" placeholder="Digite seu e-mail">
+                            </div>
+                            <div class="form-group m-1">
+                                <input class="form-control rounded" type="password" name="password" class="passwordTop" placeholder="Digite sua senha">
+                            </div>
+                            <button class="btn btn-outline-success m-1" type="submit" class="btn btn-default">Login</button>
+                        </form>
+                        <div class="dropdown-divider">
+                            <a class="dropdown-item" href="form-cadastro.php">Novo por aqui? Cadastre-se!</a>
+                            <a class="dropdown-item" href="#"> Esqueceu a senha?</a>
+                        </div>
+                    </div>
+                </div>
                 <!--fim -menu collapse-->
 
                 <!--inicio formulario entrar-->
-                <form class="navbar-form" role="login">
-                    <div class="d-flex">
-                        <div class="form-inline">
-                            <input class=" form-control rounded mr-lg-1" type="text" name="email" id="emailTop" placeholder="Digite seu e-mail">
-                            <input class=" form-control rounded mr-lg-1" type="password" name="password" id="passwordTop" placeholder="Digite sua senha">
-                            <button class="btn btn-outline-success ml-lg-1" type="submit" class="btn btn-default">Login</button>
-                        </div>
-                </form>
-                <!--fim formulario entrar-->
-
-                <!--botao cadastrar-->
-                <div class="collapse navbar-collapse" id="nav-principal">
-                    <div class="nav-item">
+                <div class="d-none d-lg-block d-xl-block">
+                    <form class="navbar-form" role="login">
+                        <div class="d-flex">
+                            <div class="form-inline">
+                                <input class=" form-control rounded mr-lg-1" type="text" name="email" class="emailTop" placeholder="Digite seu e-mail">
+                                <input class=" form-control rounded mr-lg-1" type="password" name="password" class="passwordTop" placeholder="Digite sua senha">
+                                <button class="btn btn-outline-success ml-lg-1" type="submit" class="btn btn-default">Login</button>
+                            </div>
+                    </form>
+                    <!--botao cadastrar-->
+                    <div class="nav-item ml-4">
                         <a href="#cadastrar" class="btn btn-outline-success ml-1">
                             Cadastre-se
                         </a>
                     </div>
+                    <!-- fim botao cadastrar-->
                 </div>
-            </div>
-            <!-- fim botao cadastrar-->
+                <!--fim formulario entrar-->
             </div>
         </nav>
     </header>
