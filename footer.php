5e56afede41aa296f46118f2a10b692c1b07cd95
maria junqueira <cilolata@gmail.com>
2019-07-26 17:41:33 -0300

    adding profile page

--- footer.php
+++ footer.php
@@ -1,35 +1,18 @@
  <!--incio footer-->
  <footer class="container-fluid">
-    <div class="row bg-light">
-        <div class="col-6 offset-sm-2 col-sm-2">
-            <ul class="list-unstyled">
-                <img src="img/light-bulb (4).png" alt="imagem_logo" id="logoFooter" class="d-block" width="20px" height="20px">
-                <li><a href=""></a></li>
-            </ul>
-        </div>
-        <div class="col-6 col-sm-2">
-            <ul class="list-unstyled">
-                <li><a href=""><a href=""><i class="fas fa-mail-bulk fa-2x"></i></a></li>
-                <li><a href="" role="button"></a></li>
-                
-            </ul>
-        </div>
-        <div class="col-6 col-sm-2">
-            <ul class="list-unstyled">
-                <li><a href="https://pt-br.facebook.com/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a></li>
-                <li><a href="https://pt-br.facebook.com/" target="_blank"></a></li>
-            </ul>
-        </div>
-        <div class="col-6 col-sm-2">
-            <ul class="list-unstyled">
-                <li><a href="https://www.instagram.com/?hl=pt-br" target="_blank"><i class="fab fa-instagram fa-2x"></i></a></li>
-                <li><a href="https://www.instagram.com/?hl=pt-br" target="_blank"></a></li>
-            </ul>
-        </div>
-        
-    </div>
-    <div class="row">
-        <div id="copyright" class="small col-12 text-center">Direitos Reservados <a href="https://www.flaticon.com/" title="Rede Social">Rede Social</a>&copy; 2019</div>
-    </div>
-</footer>
-<!--fim footer-->
+         <div class="col-12">
+             <ul class="list-unstyled">
+                 <div>
+                     <div class="d-flex flex-row justify-content-center">
+                         <li class="mr-2"><a href="contact-us.php"><i class="fas fa-mail-bulk fa-2x"></i></a></li>
+                         <li class="mr-2"><a href="https://pt-br.facebook.com/" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a></li>
+                         <li class="mr-2"><a href="https://www.instagram.com/?hl=pt-br" target="_blank"><i class="fab fa-instagram fa-2x"></i></a></li>
+                     </div>
+                 </div>
+             </ul>
+         </div>
+     <div class="row">
+         <div id="copyright" class="small col-12 text-center">Direitos Reservados <a href="https://www.flaticon.com/" title="Rede Social">Rede Social</a>&copy; 2019</div>
+     </div>
+ </footer>
+ <!--fim footer-->
---
+++ profile.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--bootstrap css-->
+    <script src="https://kit.fontawesome.com/b90a86aab4.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Chivo:700&display=swap" rel="stylesheet">
+    <title>Profile</title>
+</head>
+
+<body>
+    <!--inicio cabeçalho-->
+    <header>
+        <nav class="navbar fixed-top navbar-expand-md navbar-light bg-light shadow-sm p-3 mb-5">
+            <div class="container">
+
+                <!--cabeçalho logo e nome--->
+                <a href="index.php" class="navbar-brand d-flex align-items-center">
+                    <img src="img/light-bulb (4).png" alt="imagem_logo" class="d-inline-block">
+                    <span>Nome da Rede</span>
+                </a>
+                <!--fim cabeçalho logo e nome-->
+
+                <!--menu collapse-->
+
+                <!--fim -menu collapse-->
+                <div class="media">
+                    <div class="d-none d-lg-block d-xl-block">
+                        <span class="mr-3"><a class="text-success" href="">Minhas Comunidades</a></span>
+                        <span class="mr-3"><a class="text-success" href=""> Mensagens</a></span>
+                        <span class="mr-3"><a class="text-success" href="">Perfil</a></span>
+                        <!--botao cadastrar-->
+                        <img class="ml-3" style="width:50px;" src="img/team.png" alt="Imagem de exemplo genérica">
+                    </div>
+                </div>
+            </div>
+    </header>
+    <main style="margin-top:200px;">
+        <section>
+            <h4 class="text-center text-capitalize font-weight-light">Ache um grupo ou atividade e junte-se para transformar a sua comunidade</h4>
+            <div class="d-flex mt-4">
+                <div class="input-group d-flex justify-content-center">
+                    <input type="text" class="input-group-text bg-white" style="width:500px;" id="" placeholder="Pesquisar">
+                   <button type="button" class="btn btn-link"><img src="img/search.png" style="width:40px;" class="ml-2" alt=""></button>
+                </div>
+            </div>
+
+
+
+        </section>
+    </main>
+
+</body>
+
+</html>
