2022eb0250511840ea0e4a5f3c5d9c530c58ec98
ValMaier <burguipsi@hotmail.com>
2019-07-27 16:13:01 -0300

    Atualizaçao do About_us

--- about_us.php
+++ about_us.php
@@ -1,9 +1,13 @@
 <?php require_once("head.php"); ?>
 <?php require_once("header.php"); ?>
 
-
-<main> 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia vitae metus quis bibendum. Suspendisse potenti. In hac habitasse platea dictumst. In placerat rhoncus interdum. Aenean finibus, eros vel tempor congue, ante risus blandit turpis, sed scelerisque risus nisl et nunc. Maecenas rhoncus aliquam libero, vel volutpat arcu. Sed rhoncus nec velit eget molestie. Phasellus tincidunt elementum lacus, in venenatis sem pulvinar sit amet. Donec convallis tristique velit, ut interdum est porttitor sit amet. Aliquam posuere sapien turpis, eu feugiat orci laoreet eget. Donec mattis viverra erat, eget ultricies leo vulputate eu. Nullam consectetur, orci sit amet volutpat egestas, turpis tortor consequat tortor, et posuere metus tellus nec dui. Suspendisse mattis mauris arcu, vitae efficitur tortor fringilla ac.
+<main>
+    <section id="contact-us">
+        <article class="container">
+            <div class="row">
+            <div class="col-12 col-lg-6 about_us">
+            <h1 class="">About Us</h1>
+                    <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lacinia vitae metus quis bibendum. Suspendisse potenti. In hac habitasse platea dictumst. In placerat rhoncus interdum. Aenean finibus, eros vel tempor congue, ante risus blandit turpis, sed scelerisque risus nisl et nunc. Maecenas rhoncus aliquam libero, vel volutpat arcu. Sed rhoncus nec velit eget molestie. Phasellus tincidunt elementum lacus, in venenatis sem pulvinar sit amet. Donec convallis tristique velit, ut interdum est porttitor sit amet. Aliquam posuere sapien turpis, eu feugiat orci laoreet eget. Donec mattis viverra erat, eget ultricies leo vulputate eu. Nullam consectetur, orci sit amet volutpat egestas, turpis tortor consequat tortor, et posuere metus tellus nec dui. Suspendisse mattis mauris arcu, vitae efficitur tortor fringilla ac.
 
 Phasellus ultrices hendrerit quam, vitae fringilla leo feugiat ut. Etiam fermentum pretium venenatis. Proin mollis bibendum risus, eget imperdiet massa rutrum sit amet. Phasellus faucibus tincidunt ex, eu dictum mi volutpat vitae. Curabitur nunc ante, sagittis vel fermentum id, suscipit in erat. Quisque scelerisque est nec metus placerat tristique. In hac habitasse platea dictumst. Donec eleifend, enim et ullamcorper vestibulum, neque ipsum ultricies augue, non vestibulum magna ex sed mi. Integer nec interdum sem, non interdum ex. Vestibulum tristique dolor at vulputate varius.
 
@@ -13,7 +17,9 @@ Duis nulla lectus, varius vestibulum vulputate ac, posuere ac purus. Curabitur a
 
 Phasellus ac pulvinar justo. Proin vel ullamcorper leo. Aenean molestie, erat sagittis mollis congue, velit quam consequat neque, sit amet volutpat sapien magna non odio. Fusce tortor massa, venenatis id purus ut, dignissim fringilla justo. Maecenas eu est euismod lorem pulvinar sodales id eget lorem. Nulla mattis, magna eu cursus commodo, neque lectus ornare purus, id egestas justo sem nec magna. Vivamus suscipit cursus lacinia. Ut maximus leo eget libero hendrerit maximus. Proin venenatis, ex eget consectetur pellentesque, diam urna euismod augue, vitae lacinia lorem neque id quam. In sapien purus, malesuada et erat sed, consequat ultricies diam.
 
-Generated 5 paragraphs, 458 words, 3114 bytes of Lorem Ipsum
+Generated 5 paragraphs, 458 words, 3114 bytes of Lorem Ipsum!</p>
+            </div>
+            </div>
 </main>
 
 
