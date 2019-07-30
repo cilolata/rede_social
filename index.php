98a14e844ec8b59a6386e4cb686710bfc170a8f0
maria junqueira <cilolata@gmail.com>
2019-07-30 20:47:40 -0300

    adding

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 
 <main class="container">
     <!-- inicio section destaque-->
-<<<<<<< HEAD
     <section id=" destaque">
             <div class="row">
                 <div class="col-12 col-lg-8">
@@ -12,14 +11,6 @@
                     <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat obcaecati sint hic maiores est deleniti consectetur minima asperiores dolorum quas nulla consequatur, eligendi veritatis, culpa ea iure suscipit ducimus deserunt!</p>
                     <a class="btn btn-outline-success" href="about-us.php" role="button">Leia mais</a>
                 </div>
-=======
-    <section id="destaque" class="row">
-        <div class="col-12 col-lg-8">
-            <h1 class="display-5 display-sm-1">Conecte-se com pessoas que querem mudar o mundo</h1>
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat obcaecati sint hic maiores est deleniti consectetur minima asperiores dolorum quas nulla consequatur, eligendi veritatis, culpa ea iure suscipit ducimus deserunt!</p>
-            <a class="btn btn-outline-success" href="about_us.php" role="button">Leia mais</a>
-        </div>
->>>>>>> 9c3d309482142a8fbfd403c04f9e86e38e280239
 
         <div class="imagem-principal col-4 d-none d-lg-block">
             <div class="d-flex justify-content-center">
@@ -30,7 +21,7 @@
     <!-- fim section destaque-->
 
    <!-- início section cadastrar-->
-<<<<<<< HEAD
+
 <section id="cadastrar">
     <div class="container-fluid row d-flex cadastrar-container align-items-center">
                 
@@ -58,47 +49,9 @@
                     <label for="password" class="col-sm-6 col-12">Senha:
                         <input type="password" class="form-control" id="password" name="password" required>
                     </label>
-=======
-    <section id="cadastrar">
-        <div class="container-fluid row d-flex cadastrar-container align-items-center">
-                    
-            <div class="col-lg-6 d-none d-lg-block">
-                <div class="d-flex justify-content-center ">
-                    <img class="imagem2" src="img/connection.png" style="width:50%" alt="imagem-conexão" class="img_form aling-self-center">
->>>>>>> 9c3d309482142a8fbfd403c04f9e86e38e280239
                 </div>
             </div>
-            <div class="col-lg-4 col-12">
-                <h3>Junte-se à nós!</h3>
-                <p>Cadastre-se</p>
-                <form class="" action="" method="POST">
-                    <div class="form-group d-flex">
-                        <label purpose="float" for="nome" class="col-sm-6 col-12">Nome:
-                            <input type="text" class="form-control" id="nome" name="nome" required>
-                        </label>
-                        <label for="sobrenome" class="col-sm-6 col-12">Sobrenome:
-                            <input type="text" class="form-control" id="sobrenome" name="sobrenome" required>
-                        </label>
-                    </div>
-                    <div class="form-group d-flex">
-                        <label for="email" class="col-sm-6 col-12">Email:
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </label>
-                        <label for="password" class="col-sm-6 col-12">Senha:
-                            <input type="password" class="form-control" id="password" name="password" required>
-                        </label>
-                    </div>
-                    <div class="form-group d-flex">
-                        <label for="birthday" class="col-12">Nascimento:
-                            <input type="date"  class="form-control col-12" id="birthday" name="birthday" required>
-                        </label>
-                        <div class="button ml-auto col-12" id="btnSubmitLabel">
-                            <button class="btn btn-success" type="submit">Cadastrar</button>
-                        </div>
-                    </div>
-                </form>
-        </div>
-    </section>
+           </section>
         <!-- fim section cadastrar -->
 
     <!--começo section anuncios-->
