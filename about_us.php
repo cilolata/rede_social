e41d3594d30a6c73a2f9bbe446e929b1aa415a4e
ValMaier <burguipsi@hotmail.com>
2019-07-29 21:28:03 -0300

    Atualizacao no About_us

--- about_us.php
+++ about_us.php
@@ -1,41 +1,17 @@
 <?php require_once("head.php"); ?>
 <?php require_once("header.php"); ?>
 
-<main>
-    <section id="contact-us">
-        <article class="container">
-            <div class="row">
-            <div class="d-flex flex-row justify-content mr-2">
-            <div class="col-12 col-lg-6 mr-2 about_us">
-            <h1 class="mt-5">About Us</h1>
-                    <p class="text-justify">Somos uma Ong brasileira dedicada à conservação da natureza. A nossa proposta
-                    se baseia em juntar pessoas com um mesmo propósito: o de limpar lugares públicos, tais como praças, parques
-                    lugares que podem ser reaproveitados pela comunidade, se não fossem expostos a tantos entulhos, lixos, despejados
-                    por empresas e mesmo por indivíduos. Para tanto nos baseamos numa teoria: A Teoria das Janelas quebradas.
-                    <strong>Fixing Broken Windows: Restoring Order and Reducing Crime in Our Communities</strong> de George L. Kelling e Catherine Coles é um livro de criminologia
-                     e sociologia urbana publicado em 1996, sobre crime e estratégias para o conter ou eliminar dos ambientes urbanos.
-                     O livro é baseado num artigo com o título "Broken Windows" de James Q. Wilson e George L. Kelling, que surgiu em março de 1982 no The Atlantic Monthly.
-                      O título provém dos seguintes exemplos:
-                     "Considere um edifício com algumas janelas quebradas. Se as janelas não forem reparadas,
-                     a tendência é que vândalos quebrem mais janelas. Após algum tempo, poderão entrar no edifício e, se ele estiver desocupado, 
-                     torna-se uma "ocupação" ou até incendeiam o edifício, ou, Considere uma calçada ou passeio no qual algum lixo está acumulado.
-                     Ao longo do tempo, mais lixo é acumulado. No final das contas, as pessoas começam a deixar lá seus sacos de lixo."
-                     Uma estratégia de êxito para prevenir o vandalismo, dizem os autores do estudo, é resolver os problemas quando eles são pequenos. 
-                     Reparar as janelas quebradas em pouco tempo, dizem os autores, e ver-se-á que os vândalos terão menos probabilidade de estragar mais.
-                      Limpar os passeios e a tendência será de o lixo não acumular.  </p>
-            </div>
-            </div>
-            </div>
-
-            <div class="row">
-                <div class="d-flex flex-row justify-content mr-2">
-                <div class="imagem-principal col-12 col-lg-6 mr-2">
-                <img src="https://www.consulram.com/galeriassubsecciones/macro_galeria_subseccion_5.jpg" style="width:50%;"alt="justify-content-center" target="_blank"></a>
-                </div>
-            </div>
-            </div>
-                
-
+<main class="container">
+    <section class="row">
+    <h1 class="col-12" style="margin-top: 120px;">About Us</h1>
+        <article class="col-12 col-md-6">
+            <p class="text-justify">Somos uma Rede Social brasileira dedicada à ações que as pessoas quiserem realizar na comunidadd. A nossa proposta se baseia em juntar pessoas com um mesmo propósito: o de limpar lugares públicos, tais como praças, parques lugares que podem ser reaproveitados pela comunidade, se não fossem expostos a tantos entulhos, lixos, despejados por empresas e mesmo por indivíduos. Para tanto nos baseamos numa teoria: A Teoria das Janelas quebradas. <strong>Fixing Broken Windows: Restoring Order and Reducing Crime in Our Communities</strong> de George L. Kelling e Catherine Coles é um livro de criminologia  e sociologia urbana publicado em 1996, sobre crime e estratégias para o conter ou eliminar dos ambientes urbanos.  O livro é baseado num artigo com o título "Broken Windows" de James Q. Wilson e George L. Kelling, que surgiu em março de 1982 no The Atlantic Monthly.</p>
+            <p class="text-justify">O título provém dos seguintes exemplos:  "Considere um edifício com algumas janelas quebradas. Se as janelas não forem reparadas,  a tendência é que vândalos quebrem mais janelas. Após algum tempo, poderão entrar no edifício e, se ele estiver desocupado,   torna-se uma "ocupação" ou até incendeiam o edifício, ou, Considere uma calçada ou passeio no qual algum lixo está acumulado.  Ao longo do tempo, mais lixo é acumulado. No final das contas, as pessoas começam a deixar lá seus sacos de lixo."  Uma estratégia de êxito para prevenir o vandalismo, dizem os autores do estudo, é resolver os problemas quando eles são pequenos.   Reparar as janelas quebradas em pouco tempo, dizem os autores, e ver-se-á que os vândalos terão menos probabilidade de estragar mais.   Limpar os passeios e a tendência será de o lixo não acumular.</p>
+        </article>
+        <article class="col-12 col-md-6">
+            <img src="img/hands.png" style="width:50%;" alt="justify-content-center">
+        </article> 
+    </section>
 </main>
 
 
