cd2183510bd9cbaf1576a80d6ff77b366c8821c6
maria junqueira <cilolata@gmail.com>
2019-09-17 15:05:56 -0300

    layout pag index e layout pag profile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,10 @@
 @section('title', 'Together')
 @section('content')
 
-<main class="container">
+<main>
 
   <!-- inicio section destaque-->
-  <section id="destaque" class="d-flex align-items-center">
+  <section id="destaque" class="d-flex align-items-center container">
     <article class="row">
       <div class="col-12 col-lg-6 mt-5">
         <h1 class="display-5 display-sm-1">Conecte-se com pessoas que querem mudar o mundo</h1>
@@ -18,7 +18,7 @@
           fuga illum iste suscipit dicta! Suscipit reprehenderit modi aliquam itaque aliquid, a sunt
           architecto maiores perferendis ratione.
         </p>
-      <a class="btn btn-outline-secondary btn-md" href="{{url('register')}}" role="button">Cadastre-se</a>
+        <a class="btn btn-outline-secondary btn-md" href="{{url('register')}}" role="button">Cadastre-se</a>
       </div>
       <div class="imagem-principal col-6 d-none d-lg-block">
         <div class="d-flex justify-content-center">
@@ -26,8 +26,16 @@
         </div>
     </article>
   </section>
-  
-  <section id="destaque2" class="d-flex">
+
+  <!--section caixa-destaque-1-->
+  <section class="">
+    <div class="caixa-destaque1">
+    </div>
+  </section>
+  <!--fim caixa-destaque1-->
+
+  <!--section destaque1-->
+  <section id="destaque1" class="d-flex container">
     <article class="row">
       <div class="d-flex justify-content-center align-items-center">
         <div class="imagem-secundaria col-6 d-none d-lg-block">
@@ -40,56 +48,170 @@
             fuga illum iste suscipit dicta! Suscipit reprehenderit modi aliquam itaque aliquid, a sunt
             architecto maiores perferendis ratione.
           </p>
-          <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe error corrupti repellendus maxime corporis reiciendis accusantium tempore omnis minima velit,
+          <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe error corrupti
+            repellendus maxime corporis reiciendis accusantium tempore omnis minima velit,
             a vel blanditiis, nam itaque eos temporibus illum qui provident?</p>
-            <a class="btn btn-outline-secondary" href="aboutus" role="button">Leia mais</a>
+          <a class="btn btn-outline-secondary" href="aboutus" role="button">Leia mais</a>
+        </div>
+      </div>
+    </article>
+  </section>
+  <!-- fim section destaque1-->
+
+  <!--section caixa-destaque-2-->
+  <section class="">
+    <div class="caixa-destaque2">
+    </div>
+  </section>
+  <!--fim section destaque-2-->
+
+
+<!--section eventos-->
+  <section class="minhas_comunidades mt-5 d-none d-lg-block d-xl-block container">
+    <h5 class="mt-5">Eventos</h5>
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-3 cardAnuncioCont">
+        <div class="card d-block">
+          <img class="card-img-top" src="img\imagem1.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Título do card</h5>
+            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer
+              preencher o conteúdo do card.</p>
+            <a href="#" class="btn btn-outline-secondary">Visitar</a>
           </div>
         </div>
-      </article>
-    </section>
-    <!-- fim sections destaque-->
-
-    <!--section imagens-->
-
-    
-
-    <!--fim imagens-->
-    
-  <!--inicio cards eventos -->
-  <section class="container mt-5 div-card-anuncios d-none d-lg-block d-xl-block">
-    <h3>Alguns dos nossos eventos</h3>
-    <div class="row d-flex justify-content-around mt-5">
-      <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Título do card</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Subtitulo do card</h6>
-          <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo
-            do card.</p>
-          <a href="#" class="card-link">Link do card</a>
-          <a href="#" class="card-link">Outro link</a>
+      </div>
+      <div class="col-12 col-sm-6 col-md-3 cardAnuncioCont">
+        <div class="card d-block">
+          <img class="card-img-top" src="img\imagem1.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Título do card</h5>
+            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer
+              preencher o conteúdo do card.</p>
+            <a href="#" class="btn btn-outline-secondary">Visitar</a>
+          </div>
         </div>
       </div>
-      <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Título do card</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Subtitulo do card</h6>
-          <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo
-            do card.</p>
-          <a href="#" class="card-link">Link do card</a>
-          <a href="#" class="card-link">Outro link</a>
+      <div class="col-12 col-sm-6 col-md-3 cardAnuncioCont">
+        <div class="card d-block">
+          <img class="card-img-top" src="img\imagem1.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Título do card</h5>
+            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer
+              preencher o conteúdo do card.</p>
+            <a href="#" class="btn btn-outline-secondary">Visitar</a>
+          </div>
         </div>
       </div>
-      <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Título do card</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Subtitulo do card</h6>
-          <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo
-            do card.</p>
-          <a href="#" class="card-link">Link do card</a>
-          <a href="#" class="card-link">Outro link</a>
+      <div class="col-12 col-sm-6 col-md-3 cardAnuncioCont">
+        <div class="card d-block">
+          <img class="card-img-top" src="img\imagem1.jpg" alt="Imagem de capa do card">
+          <div class="card-body">
+            <h5 class="card-title">Título do card</h5>
+            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer
+              preencher o conteúdo do card.</p>
+            <a href="#" class="btn btn-outline-secondary">Visitar</a>
+          </div>
         </div>
       </div>
-      <!--fim cards eventos-->
+    </div>
+  </section>
+
+  <!-- inicio carousel -->
+  <section class="d-xl-none d-lg-none m-4 div-card-anuncios">
+
+    <article>
+      <h5 class="ml-3 mb-3">Eventos</h5>
+      <div class="d-flex flex-row">
+        <!--Carousel Wrapper-->
+        <div id="myCarousel" class="carousel slide carousel-multi-item" data-ride="carousel">
+
+          <!--Indicators-->
+          <ol class="carousel-indicators">
+            <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
+            <li data-target="#multi-item-example" data-slide-to="1"></li>
+            <li data-target="#multi-item-example" data-slide-to="2"></li>
+          </ol>
+          <!--/.Indicators-->
+
+          <!--Slides-->
+          <div class="carousel-inner" role="listbox">
+
+            <!--First slide-->
+            <div class="carousel-item active">
+              <div class="col-md-4">
+                <div class="card mb-2">
+                  <img class="card-img-top" src="img/imagem1.jpg" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h4 class="card-title">Card 1</h4>
+                    <p class="card-text">Um exemplo de texto rápido para construir o título do card
+                      e fazer preencher o conteúdo do card.</p>
+                    <a href="#" class="btn btn-secondary">Visitar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--/.First slide-->
+
+            <!--Second slide-->
+            <div class="carousel-item">
+              <div class="col-md-4">
+                <div class="card mb-2">
+                  <img class="card-img-top" src="img/imagem1.jpg" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h4 class="card-title">Card 2</h4>
+                    <p class="card-text">Um exemplo de texto rápido para construir o título do card
+                      e fazer preencher o conteúdo do card.</p>
+                    <a href="#" class="btn btn-secondary">Visitar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--/.Second slide-->
+
+            <!--Third slide-->
+            <div class="carousel-item">
+              <div class="col-md-4">
+                <div class="card mb-2">
+                  <img class="card-img-top" src="img/imagem1.jpg" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h4 class="card-title">Card 3</h4>
+                    <p class="card-text">Um exemplo de texto rápido para construir o título do card
+                      e fazer preencher o conteúdo do card.</p>
+                    <a href="#" class="btn btn-secondary">Visitar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--/.Third slide-->
+
+            <!--Fourth slide-->
+            <div class="carousel-item">
+              <div class="col-md-4">
+                <div class="card mb-2">
+                  <img class="card-img-top" src="img/imagem1.jpg" alt="Imagem de capa do card">
+                  <div class="card-body">
+                    <h4 class="card-title">Card 4</h4>
+                    <p class="card-text">Um exemplo de texto rápido para construir o título do card
+                      e fazer preencher o conteúdo do card.</p>
+                    <a href="#" class="btn btn-secondary">Visitar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--/.Fourth slide-->
+
+          </div>
+          <!--/.Slides-->
+
+        </div>
+        <!--/.Carousel Wrapper-->
+
+      </div>
+    </article>
+  </section>
+
+
 
 
 </main>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,68 +7,65 @@
 
 <main class="container mt-5 p-5">
   <section class="d-flex row">
-      <form method="POST" action="/alterarUsuario" enctype="multipart/form-data">
+    <form method="POST" action="/alterarUsuario" enctype="multipart/form-data">
 
-        @csrf
-        {{ method_field('POST') }}
+      @csrf
+      {{ method_field('POST') }}
 
-        <div class="form-row">
+      <div class="form-row">
+        <aside class="col-lg-2 col-sm-12">
+          <img src="https://i.pinimg.com/originals/b9/58/5a/b9585a9d1b20060f0d7e83c72eaa159f.jpg"
+            class="rounded-circle img-profile w-75">
+          <input type="file" class="mt-3" id="imagem" name="imagem"
+            class="form-control @error('imagem') is-invalid @enderror" name="imagem" value="{{ old('imagem') }}"
+            required autocomplete="imagem" autofocus>
 
-          <aside class="col-lg-2 col-sm-12">
-            <img src="https://i.pinimg.com/originals/b9/58/5a/b9585a9d1b20060f0d7e83c72eaa159f.jpg" class="rounded-circle img-profile w-75">
-            <input type="file" id="imagem" name="imagem" class="form-control @error('imagem') is-invalid @enderror" name="imagem" value="{{ old('imagem') }}" required autocomplete="imagem" autofocus>
+          @error('imagem')
+          <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+          </span>
+          @enderror
+        </aside>
 
-            @error('imagem')
-              <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-              </span>
-            @enderror
-          </aside>
+        <div class="col-12 mt-4">
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <label for="name">Nome</label>
+              <input type="text" class="form-control" id="name" placeholder="Nome" value="{{$usuario->name}}" required>
+            </div>
 
-          <div class="col-md-4 mb-3">
-            <label for="name">Nome</label>
-            <input type="text" class="form-control" id="name" placeholder="Nome" value="{{$usuario->name}}" required>
+            <div class="col-md-4 mb-3">
+              <label for="sobrenome">Sobrenome</label>
+              <input type="text" class="form-control" id="sobrenome" placeholder="Sobrenome"
+                value="{{$usuario->sobrenome}}" required>
+            </div>
+
+            <div class="col-md-3 mb-2">
+              <label for="CEP">CEP</label>
+              <input type="text" class="form-control" id="CEP" maxlength="9" placeholder="CEP" required name="CEP"
+                value="{{$usuario->cep}}">
+            </div>
+
+            <div class="col-md-3 mb-2">
+              <label for="estado">Estado</label>
+              <input type="text" class="form-control" id="UF" placeholder="Estado" required name="estado"
+                value="{{$usuario->estado}}">
+            </div>
+
+            <div class="col-md-3 mb-2">
+              <label for="cidade">Cidade</label>
+              <input type="text" class="form-control" id="cidade" placeholder="Cidade" name="cidade"
+                value="{{$usuario->cidade}}" required>
+            </div>
           </div>
+            <button class="btn btn-secondary mt-4" type="submit">Enviar</button>
 
-          <div class="col-md-4 mb-3">
-            <label for="sobrenome">Sobrenome</label>
-            <input type="text" class="form-control" id="sobrenome" placeholder="Sobrenome"
-              value="{{$usuario->sobrenome}}" required>
-          </div>
-
-          <div class="col-md-3 mb-2">
-            <label for="CEP">CEP</label>
-            <input type="text" class="form-control" id="CEP" maxlength="9" placeholder="CEP" required name="CEP" value="{{$usuario->cep}}">
-          </div>
-
-          <div class="col-md-3 mb-2">
-            <label for="estado">Estado</label>
-            <input type="text" class="form-control" id="UF" placeholder="Estado" required name="estado" value="{{$usuario->estado}}">
-          </div>
-
-          <div class="col-md-3 mb-2">
-            <label for="cidade">Cidade</label>
-            <input type="text" class="form-control" id="cidade" placeholder="Cidade" name="cidade" value="{{$usuario->cidade}}" required>
           </div>
         </div>
-
-        <div class="form-row">
-
-          <button class="btn btn-secondary mt-4" type="submit">Enviar</button>
-
-        </div>
+      </div>
     </form>
   </section>
 </main>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-</script>
-<script src="{{url("js/main.js")}}"></script>
+
 @endsection
